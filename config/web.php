fcf7fa80a79d180189deb4c96af4b4b11744c44c
Locustv2 <locustv2@gmail.com>
2021-01-11 18:16:35 +0100

    Added urlmanager to web config

--- config/web.php
+++ config/web.php
@@ -12,5 +12,17 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'rules' => [
+                '' => 'site/index',
+                '<controller:[\w+\-]+>' => '<controller>/index',
+                '<controller:[\w+\-]+>/<id:[\d]+>' => '<controller>/view',
+                '<controller:[\w+\-]+>/<action:[\w+\-]+>/<id:[\d]+>' => '<controller>/<action>',
+                '<controller:[\w+\-]+>/<action:[\w+\-]+>' => '<controller>/<action>',
+            ],
+        ],
     ],
 ];
